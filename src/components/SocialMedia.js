dd8a96aa49c90c2f8ba30fc880f60c76865d2f67
Ranvijay Kumar <rvkofficial1@gmail.com>
2024-07-15 22:49:35 +0530

    portfolio created

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,44 @@
+const SocialMedia = () => {
+    return (
+      <div className="">
+        <h1 className="text-center font-bold text-[20px]">Where to find me ?</h1>
+<div className="flex justify-center md:justify-start my-5 gap-7">
+        <a
+          className="w-8 h-8 hover:opacity-50 transition duration-500"
+          href="https://www.linkedin.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="linkedin-link" src="/linkedin.png" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://leetcode.com/phoenix_rvk"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="leetcode" src="/leetcode.png" className="w-8 h-8 md:hover:scale-110 rounded-md" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="#"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="facebook-link" src="/codeforces.png" className="w-8 h-8 md:hover:scale-110 rounded-md" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.github.com/rvk7021"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="instagram-link" src="/github.jpeg" className="w-8 h-8 md:hover:scale-110 rounded-md" />
+        </a>
+      </div>
+      </div>
+      
+    );
+  };
+  
+  export default SocialMedia;
---
+++ src/constants/Data.js
@@ -0,0 +1,172 @@
+export const Data = [
+    {
+      id: '1',
+      title: 'Class X',
+      date:'2019',
+      subtitle:'Podar International School, Jalna',
+      icon: <img src="/school.png" className='rounded-full' alt='school'/>,
+      score: '86.4%',
+      description: `I attended Podar International School, where I studied under the CBSE board curriculum. Through dedicated effort and commitment to my studies, I achieved a score of {score} in my board examinations. This accomplishment reflects my strong academic foundation and consistent performance across subjects.`
+    },
+    {
+        id: '2',
+        title: 'Higher Secondary Education',
+        date:'2019-2021',
+        subtitle:'Yogiraj Public School, Kota',
+        icon: <img src="/twelthschool.jpeg" className='rounded-full' alt='school'/>,
+        score: '82.5%',
+        description: `Attended Yogiraj Public School, Kota for Higher Secondary Education with a PCM major in the CBSE curriculum. 
+        Gained solid skills in scientific and mathematical concepts.`
+    },
+    {
+    id:'3',
+    date: '2022 - 2026',
+    icon: <img src="/college.jpeg" className='rounded-full' alt='college' />,
+    score:'Grade: 8.25',
+    title: 'Graduation',
+    subtitle: 'B.Tech In CSE, IIIT Nagpur',
+    description: `I am currently pursuing a B.Tech in Computer Science at the Indian Institute of Information Technology. 
+                    Through this program, I am gaining a strong foundation in core computer science principles while also honing my coding skills. 
+                    My coursework and projects allow me to explore various programming languages, data structures, algorithms, and software development methodologies, preparing me for a career in the tech industry.`
+      }
+  ];
+
+  export const JobData = [
+    {
+      company: 'TechCorp',
+      position: 'Senior Developer',
+      duration: 'Jan 2020 - Present',
+      points: [
+        'Led a team to build scalable web apps with modern frameworks.',
+        'Implemented CI/CD pipelines for efficient development.',
+        'Optimized performance, reducing load times by 30%.',
+      ],
+    },
+    {
+      company: 'Innovatech',
+      position: 'Frontend Developer',
+      duration: 'Jun 2017 - Dec 2019',
+      points: [
+        'Developed responsive web interfaces using React and Redux.',
+        'Collaborated with designers for seamless user experiences.',
+        'Implemented automated testing for code quality.',
+      ],
+    },
+    {
+      company: 'WebSolutions',
+      position: 'Web Application Designer',
+      duration: 'Jan 2015 - May 2017',
+      points: [
+        'Designed user interfaces focusing on usability and aesthetics.',
+        'Conducted user research and usability testing for designs.',
+        'Created wireframes and mockups using Sketch and Figma.',
+      ],
+    },
+    {
+      company: 'NextGen Technologies',
+      position: 'Full Stack Developer',
+      duration: 'Mar 2013 - Dec 2014',
+      points: [
+        'Built full-stack web apps with JavaScript, Node.js, and MongoDB.',
+        'Integrated APIs to enhance application functionality.',
+        'Implemented authentication and authorization mechanisms.',
+      ],
+    },
+    {
+      company: 'CodeCrafters',
+      position: 'Backend Developer',
+      duration: 'Jul 2010 - Feb 2013',
+      points: [
+        'Designed RESTful APIs using Node.js and Express.',
+        'Optimized database queries for better performance.',
+        'Ensured data security through encryption and access controls.',
+      ],
+    },
+    {
+      company: 'Creative Minds',
+      position: 'Junior Developer',
+      duration: 'Jan 2008 - Jun 2010',
+      points: [
+        'Developed web apps with HTML, CSS, and JavaScript.',
+        'Debugged and resolved issues in existing applications.',
+        'Assisted senior developers and learned new technologies.',
+      ],
+    },
+  ];
+  
+
+
+  export const ProjectData = [
+    {
+      id: 1,
+      title: 'Component World',
+      description: "Want components for your application, We got it, Want to contribute to community ?Let's do it",
+      image: "./uiworld.png", 
+      projectLink: 'https://example.com/project1', // Replace with actual project URLs
+      codeLink: 'https://github.com/username/project1', // Replace with actual code repository URLs
+      tags: ['Web App'],
+      techStack: ['Figma', 'Adobe XD', 'Sketch'], // Array of technologies used
+      date: '2024-05-01', 
+    },
+    {
+      id: 2,
+      title: 'Wanderlust',
+      description: 'An tourism site that offers wide range of services from bookings to information',
+      image: "./wanderlust.png",
+      projectLink: 'https://example.com/project2',
+      codeLink: 'https://github.com/username/project2',
+      tags: ['Web App', 'E-Commerce'],
+      techStack: ['HTML','CSS','PHP','SQL','JavaScript'], 
+      date: '2024-03-15',
+    },
+    {
+      id: 3,
+      title: 'WriteMate',
+      description: 'A platform for not just bloggers but for entire writing community.',
+      image: "./writemate.png",
+      projectLink: 'https://example.com/project4',
+      codeLink: 'https://github.com/username/project4',
+      tags: ['mlweb', 'Blogging'],
+      techStack: ['Vue.js', 'Express.js', 'MySQL'],
+      date: '2023-11-30',
+    },
+    
+    {
+      id: 4,
+      title: 'Portfolio Website',
+      description: 'A personalized website,the one you are scrolling right now',
+      image: "./portfolio.png",
+      projectLink: 'https://example.com/project6',
+      codeLink: 'https://github.com/username/project6',
+      tags: ['Web App', 'Personal'],
+      techStack: ['React', 'CSS', 'JavaScript',''],
+      date: '2024-07-01',
+    },
+    {
+      id: 5,
+      title: 'Web Scrapper',
+      description: 'A platform that allows you to download your favorite content offline',
+      image: "./webscrapper.png",
+      projectLink: 'https://example.com/project6',
+      codeLink: 'https://github.com/username/project6',
+      tags: ['mlweb', 'Personal'],
+      techStack: ['React', 'CSS', 'JavaScript','Python'],
+      date: '2024-07-01',
+    },
+    {
+      id:6,
+      title: 'DashCreator',
+      description: 'A platform that allows you to create a powerful dashboard like POWER BI just by inputing .csv file',
+      image: "./dashcreator.png",
+      projectLink: 'https://example.com/project6',
+      codeLink: 'https://github.com/username/project6',
+      tags: ['mlweb', 'Personal'],
+      techStack: ['React', 'CSS', 'JavaScript','Python'],
+      date: '2024-07-01',
+    },
+  ];
+  
+  
+  
+
+  
